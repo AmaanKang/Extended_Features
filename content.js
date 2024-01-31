c4c45981655050317fe7994810893a09ddb16527
Amandeep Kaur <amankang344@gmail.com>
2024-01-30 20:25:03 -0500

    Fixing the button alignment

--- content.js
+++ content.js
@@ -1,8 +1,5 @@
 // content.js
-const iconsLink = document.createElement('link');
-iconsLink.href = chrome.runtime.getURL('bootstrap-icons-1.11.3/font/bootstrap-icons.css');
-iconsLink.rel = 'stylesheet';
-document.head.appendChild(iconsLink);
+
 
 function createNotificationButton(controlBar) {
   if (!controlBar) {
@@ -12,17 +9,29 @@ function createNotificationButton(controlBar) {
   const notificationButton = document.createElement('button');
   notificationButton.classList.add('ytp-button');
   notificationButton.title = 'Set Notification';
-  var icon = document.createElement('i');
-  icon.className = 'bi-alarm-fill';
 
-  // Set the size of the icon
-  // Set the size of the icon
-  icon.style.fontSize = '24px';
-  icon.style.marginBottom = '50px';
-  icon.style.paddingBottom = '50px';
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width','100%');
+  svg.setAttribute('height','100%');
+  svg.setAttribute('fill','currentColor');
+  svg.setAttribute('class','bi bi-alarm-fill');
+  svg.setAttribute('viewBox','0 0 36 36');
 
-// Add the icon to the button
-  notificationButton.appendChild(icon);
+  var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d','M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5m2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.04 8.04 0 0 0 .86 5.387M11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.04 8.04 0 0 0-3.527-3.527');
+
+  var useStyle = document.createElement('use');
+  useStyle.className = 'ytp-svg-shadow';
+
+  svg.appendChild(useStyle);
+  svg.appendChild(path);
+  svg.paddingTop = '50px';
+
+  notificationButton.appendChild(svg);
+
+  
+  //notificationButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-alarm-fill" viewBox="0 0 36 36"><path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5m2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.04 8.04 0 0 0 .86 5.387M11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.04 8.04 0 0 0-3.527-3.527"/></svg>';
+
 
   notificationButton.addEventListener('click', function() {
     // Create a modal dialog
