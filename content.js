7dd4c127a137f800f833beafa817cc56c82b72b0
Amandeep Kaur <amankang344@gmail.com>
2024-02-05 23:58:23 -0500

    Content.js documentation

--- content.js
+++ content.js
@@ -5,11 +5,12 @@ function createNotificationButton(controlBar) {
   if (!controlBar) {
     return;
   }
-
+   //Create the notification button element
   const notificationButton = document.createElement('button');
   notificationButton.classList.add('ytp-button');
   notificationButton.title = 'Set Notification';
 
+  //Create the svg for the notification button
   var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('width','100%');
   svg.setAttribute('height','100%');
@@ -29,10 +30,7 @@ function createNotificationButton(controlBar) {
 
   notificationButton.appendChild(svg);
 
-  
-  //notificationButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-alarm-fill" viewBox="0 0 36 36"><path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5m2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.04 8.04 0 0 0 .86 5.387M11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.04 8.04 0 0 0-3.527-3.527"/></svg>';
-
-
+  //When notification button is clicked, then following method should play.
   notificationButton.addEventListener('click', function() {
     // Create a modal dialog
     const modal = document.createElement('div');
@@ -86,8 +84,7 @@ function createNotificationButton(controlBar) {
     const timePicker = document.createElement('input');
     timePicker.type = 'time';
 
-
-    // Append the date picker, time picker, and buttons to the modal content container
+    // Append the date picker, time picker, and buttons to the modal body
     modalBody.appendChild(datePicker);
     modalBody.appendChild(timePicker);
 
@@ -108,6 +105,7 @@ function createNotificationButton(controlBar) {
     modal.appendChild(modalDialog);
     document.body.appendChild(modal);
     
+    //The modal should show when the button has been clicked.
     var myModal = new bootstrap.Modal(document.getElementById('myModal'));
     myModal.show();
 
